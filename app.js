b21b99fdd687d1188904c2536fda519a347a6f6f
Sarabdeep Singh <sssingh@medline.com>
2018-10-12 10:45:38 -0500

    newwer changes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('views', 'views');
 app.set('view engine', 'pug');
 
 const commitRef = process.env.APP_COMMIT_REF || 'N/A'
-const buildDate = process.env.APP_BUILD_DATE || new Date().toISOString()
+const buildDate = process.env.APP_BUILD_DATE || new Date().toString()
 
 app.get('/', function(req, res) {
     res.render('home', {commitReference: commitRef, currentBuildDate:buildDate});
